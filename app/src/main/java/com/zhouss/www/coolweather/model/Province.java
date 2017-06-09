7252ba1ae8863e7f781f08b0f100c0528da48d9f
zs14 <1509403958@qq.com>
2017-06-09 14:38:51 +0800

    init database

---
+++ app/src/main/java/com/zhouss/www/coolweather/model/Province.java
@@ -0,0 +1,32 @@
+package com.zhouss.www.coolweather.model;
+
+/**
+ * Created by zs on 2017/6/9.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
